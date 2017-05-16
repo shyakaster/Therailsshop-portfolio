a45a3336e69349782aa6dcc8c860e997c6bd2b8e
alex shyaka <shyakaster@gmail.com>
2017-05-16 15:51:49 +0300

    Implemented authosrisation for blogs and portsfolios

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-      link_to 'Sign out', destroy_user_session_path, method: :delete
+    if current_user.is_a?(GuestUser)
+      (link_to 'Sign in', new_user_session_path) +
+      "<br>".html_safe +
+      (link_to 'Register', new_user_registration_path)
       else
-        (link_to 'Sign in', new_user_session_path) +
-        "<br>".html_safe +
-        (link_to 'Register', new_user_registration_path)
+        link_to 'Sign out', destroy_user_session_path, method: :delete
     end
   end
 
--- app/models/concerns/guest_user_concern.rb
+++ app/models/concerns/guest_user_concern.rb
@@ -5,6 +5,13 @@ module GuestUserConcern
     super || guest_user
   end
   def guest_user
-    OpenStruct.new(full_name:"Guest", first_name:"Alex", last_name:"Shyaka",email:"alex@example.com")
+      # OpenStruct.new(full_name:"Guest", first_name:"Alex", last_name:"Shyaka",email:"alex@example.com")
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@email.com"
+    guest
   end
+
 end
