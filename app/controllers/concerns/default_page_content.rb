ad244cfdda811ca23a69731cabce13cf231b4633
alex shyaka <shyakaster@gmail.com>
2017-05-21 14:29:59 +0300

    add styling to edit and new registration forms

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -5,7 +5,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-      @page_title = " Portfolio | alex shyaka"
+      @page_title = " Portfolio | #{current_user.full_name}"
       @seo_keywords = "alex shyaka porfolio"
   end
 end
