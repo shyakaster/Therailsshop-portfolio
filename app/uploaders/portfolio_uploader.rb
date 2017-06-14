576ef6780eea26825fe5cb015db968fb44f67da9
Ubuntu <ubuntu@rails-dev-box>
2017-06-14 01:30:33 +0000

    Solve the permission issue with uploading to aws on the vagrant box not working on the windows machine though

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,7 +1,8 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
-  storage :file
+  storage :aws
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+       "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+     # "#{Rails.root}/public/"
   end
   def extension_whitelist
     %w(jpg jpeg gif png)
