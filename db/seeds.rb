c49c0b9008978a1bbe04c918e97b37ac00cb3577
Ubuntu <ubuntu@rails-dev-box>
2017-07-12 12:33:47 +0000

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+User.create!(
+  email: "shyakaster@gmail.com",
+  password: "beatrice",
+  password_confirmation: "beatrice",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
+
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
 3.times do |topic|
 	Topic.create!(
 	title:"Topic #{topic}"
