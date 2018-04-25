9c902ef3737cb87ecbd2422db01aecca363fd4b9
alex shyaka <shyakaster@gmail.com>
2018-04-25 17:21:34 +0300

    sort blog issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.create!(
 
 puts "1 regular user created"
 
-
+Topic.create!(title:'Coding')
 # 3.times do |topic|
 #   Topic.create!(
 #     title: "Topic #{topic}"
@@ -29,16 +29,25 @@ puts "1 regular user created"
 # 	)
 # end
 # puts "3 topics created"
-# 10.times do |blog|
+# 2.times do |blog|
 # 		Blog.create!(
-# 		title: " My Blog post #{blog}",
-# 		body: "At vero eos et accusamus et iusto odio dignissimos ducimus
-# 		qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores
-# 		alias consequatur aut perferendis doloribus asperiores repellat",topic_id: Topic.last.id
+# 		title: " Coding for social change-Kampala ruby community",
+# 		body: "I have heard endless stories of companies some foreign and others not saying they can't find good developers in Uganda. They have to go somewhere else to get developers for jobs being done here. We have talented young people if well mentored can take up these positions. Our community hopes to attract enough young and old talent to learn and grow together and tremendously challenge this belief.
+
+# Through working with kids in my community teaching them how to code, I have come to the realization that talent is not restricted to developed countries only, it's the opportunity that is missing. The community will provide the opportunity to kids as young as 11 years old to be part of this learning process.
+
+# Social change is something that I hold dearly and  through the ruby community we will work with strategic partners to extend this opportunity to under-served areas in Kampala first and the rest of the country eventually. We shall achieve this through setting up coding camps in poorly served areas where kids may not be aware of the opportunities that are taken for granted by their well to do contemporaries
+#   Enough about the language. I will try to articulate the best way I can why we are building this community and the outcomes we expect  out of this en-devour.
+
+# As a community we hope to contribute to the wider community of the ruby language through working on open source projects especially the ones that are of an altruistic nature. We hope to learn and gain technical sophistication while we add value to the community.
+
+# This will help us gain confidence as developers here in Africa. I have realized a tendency by developers in Uganda to feel inadequate to contribute because of a lack of self confidence. We hope to encourage our members to dare to challenge themselves more and believe in their abilities to contribute.
+
+# Our regular meetups once a month will be greatly helpful in helping our members to network with industry heavy weights and experienced developers who will give talks both technical and of a business nature. We shall be resilient in making sure we add value to our members through learning difficult topics and making sure we fully understand the core of the language.",topic_id: Topic.last.id
 # 		)
 # 	end
 
-# puts "10 blogs created"
+# puts "2 blogs created"
 
 # 5.times do |skill_set|
 # 		Skill.create!(
